8f0ba402dcd5e652d3befe6090ff0cfdb4c17096
손창효 <ckdgy123@gmail.com>
2021-06-03 20:56:34 +0900

    initial commit

---
+++ my_settings.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'django-insecure-cbnh0gufwc-&y)63+qyvi46q9t4_hk=wf++o!9!!0k#hw4o87d'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'westagram_practice',
+        'USER':'root',
+        'PASSWORD':'Vs5453vs!',
+        'HOST':'localhost',
+        'PORT':3306
+    }
+}
